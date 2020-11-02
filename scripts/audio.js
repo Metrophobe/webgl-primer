e435758a826693b13b05df9ac46734c8ca2a81e6
= <=>
2020-11-02 18:38:00 +0100

    fixed audio not playing issue....

--- scripts/audio.js
+++ scripts/audio.js
@@ -2,7 +2,9 @@ let audio;
 
 let init = async () => {
     audio = await new Audio('./assets/audio/gyroscope.mp3');
+    audio.muted = true;
     audio.play();
+    audio.muted = false;
 }
 
 export default {
